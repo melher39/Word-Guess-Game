721e1f3a0583b344edba44636db4fe70c7c8a01d
Melvin Hernandez <melher39@gmail.com>
2018-10-15 22:00:51 -0500

    rough outline in html and pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+// I have to set random words that will be used as the ones being guessed by the user
+// these can possibly be stored in an array and called upon at random
+
+var seinLanguage = ["Jerry", "Kramer", "George", "Elaine", "New York", "Soup Nazi", "Assman", "Junior Mints", "Puffy Shirt", "Newman"];
+
+// testing link between html and js 
+console.log(seinLanguage[0]);
+
+// letter strokes on keyboard can be tracked by onkey or eventlistener?
+
+// incorrect and correctly guessed letters will de displayed on screen
+
+// every incorrectly guessed word will deduct from the number of guesses remaining
+
+// once word is guessed correctly, game will restart with a new word but keep track of the wins
+
