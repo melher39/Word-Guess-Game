38ea7c6bb129bee6e3d81ffa951c24e3f987f5a6
Melvin Hernandez <melher39@gmail.com>
2018-10-19 11:51:52 -0500

    created seinfeldGame object in js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,98 @@
 // I have to set random words that will be used as the ones being guessed by the user
 // these can possibly be stored in an array and called upon at random
 
-var seinLanguage = ["Jerry", "Kramer", "George", "Elaine", "New York", "Soup Nazi", "Assman", "Junior Mints", "Puffy Shirt", "Newman"];
+var seinLanguage = ["jerry", "kramer", "george", "elaine", "new york", "soup nazi", "assman", "junior mints", "puffy shirt", "newman"];
 
 // randomize the selection from seinLanguage
-var randomWord = seinLanguage[Math.floor(Math.random() * seinLanguage.length)];
+// var randomWord = seinLanguage[Math.floor(Math.random() * seinLanguage.length)];
 
 // eliminate the spaces in between words for when taking into account the number of guesses the user has left
-var randomWordNoSpace= randomWord.replace(/ +/g, "");
-var finalWord = randomWordNoSpace.toLowerCase();
+// var finalWord= randomWord.replace(/ +/g, "");
+// var finalWord = randomWordNoSpace.toLowerCase();
 
-console.log(finalWord);
+// console.log(finalWord);
 
 // testing link between html and js 
-console.log(randomWord);
+// console.log(randomWord);
 
-// declare variables
-var wins = 0;
-var losses = 0;
-var initialNumberOfGuesses = randomWordNoSpace.length;
-var correctWord = []
-var guessedKey = "";
+// declare an object for for the propery values and methods needed to play this Seinfeld game
 
-console.log(initialNumberOfGuesses)
+var seinfeldGame = {
+    wins: 0, //starts at 0 but will increase as the user plays if they win a round
+    losses: 0, //starts at 0 but will increase as the user plays if they lose a round
+    initialNumberOfGuesses: finalWord.length, //number of guesses will always equal to the number of characters in the computer randomized word, the shorter the word, the less tries the user will be given
+    finalWord: "", //this has the computer randomized correct word until the user guesses it
+    incorrectGuessedKey: "", //ths stores the wrong guessed letters the player types
+    correctGuessedKey: [], //this saves the correct guessed letters the player types
+    underlines: [], //this will store the underlines to be used as the player is guessing the computer randomized word
+    guessedKey: "", //this stores any key typed by the user
+
+    // this will show the underlines in place of the finalWord
+    showUnderlineForCorrectGuessedKey: function () {
+        for (var i=0; i < this.finalWord.length; i++) {
+            this.correctGuessedKey[i] = this.correctGuessedKey[i] + " ";
+            this.underlines = this.underlines + "_";
+        }
+    }, 
+    // function will keep track of the number of wins
+    winsCounter: function() {
+        var counter = 1; //keeps track of the correct number of guessed letters
+        for (var i=0; i < finalWord.length; i++) {
+            if (this.correctGuessedKey[i].indexOf(" " === -1)) {
+                counter++;
+            }
+            if (counter === this.finalWord.length) {
+                this.wins++;
+                this.gameReset();
+            }
+
+
+        }
+
+    },
+    //function will keep track of the number of losses
+    lossesCounter: function() {
+        if (this.initialNumberOfGuesses === 0) {
+            this.losses++;
+            this.gameReset();
+        }
+    },
+
+    //function will reset the game once the player loses or wins, but will not reset the score of losses or wins
+    gameReset: function() {
+        this.initialNumberOfGuesses = finalWord.length;
+        // this.finalWord = "";
+        this.incorrectGuessedKey = "";
+        this.correctGuessedKey = [];
+        this.underlines = [];
+        this.guessedKey = "";
+        this.showUnderlineForCorrectGuessedKey();
+
+        // randomize the selection from seinLanguage
+        var randomWord = seinLanguage[Math.floor(Math.random() * seinLanguage.length)];
+
+        // eliminate the spaces in between words for when taking into account the number of guesses the user has left
+        this.finalWord= randomWord.replace(/ +/g, "");
+    },
+    //check to see if the user guessed letter matches with one in the finalWord
+    compareGuessedKeyToFinalWord: function() {
+        for (var i=0; i<this.finalWord.length; i++){
+            if(this.finalWord.indexOf(this.guessedKey > -1)) {
+                this.correctGuessedKey[this.guessedKey];
+            }
+            else{
+                this.incorrectGuessedKey = this.incorrectGuessedKey + this.guessedKey + " ";
+                this.initialNumberOfGuesses--;
+            }
+        }
+    }
+
+
+     
+
+};
+
+console.log(seinfeldGame.initialNumberOfGuesses)
 
 // maybe use the push keyword to push guessed letters to the screen once they're guessed
 
